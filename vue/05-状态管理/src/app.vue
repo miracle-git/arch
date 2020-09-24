10d3db8ff562f40aedafd6c689e27b4320afd269
miracle <hmiinyu@sina.com>
2020-09-24 23:18:51 +0800

    feat(): update vuex

--- vue/05-状态管理/src/app.vue
+++ vue/05-状态管理/src/app.vue
@@ -4,7 +4,6 @@
     <hr>
     <p><button @click="$store.commit('add')">Add Counter</button> {{$store.state.count}}</p>
     <p><button @click="$store.dispatch('addAsync')">Add Aysnc Counter</button> {{$store.state.count}}</p>
-<!--    <p>Double Counter: {{$store.getters.doubleCount}}</p>-->
   </div>
 </template>
 
--- vue/05-状态管理/src/store/index.js
+++ vue/05-状态管理/src/store/index.js
@@ -7,11 +7,6 @@ export default new Vuex.Store({
   state: {
     count: 0
   },
-  getters: {
-    doubleCount(state) {
-      return state.count * 2
-    }
-  },
   mutations: {
     add(state) {
       state.count++
